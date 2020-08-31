395a1af5e10cd33399d25184a724cc70063b51c5
Jackie <taekwondo.chan@gmail.com>
2020-08-30 22:51:08 -0700

    Add second calculation

--- calculator.py
+++ calculator.py
@@ -6,12 +6,26 @@ while True:  # Asks for weight input until a float is entered.
     try:
         input_weight_lb = float(input("How much weight can you benchpress for 4-6 reps? ")) # Convert input into float for later usage.
     except ValueError:
-        print("Enter a valid number.") # Convert input into float for later usage.
+        print("Enter a valid number.")
         continue
     else:
         break
 
-one_rep_max_lb = (input_weight_lb * 6 * 0.033) + input_weight_lb  # Calculate one rep max.
+while True:
+    try:
+        input_rep_count = int(input("How many reps were completed at this weight? Must be 4, 5, or 6. ")) # Determine rep count to pick which calculation to perform.
+        if input_rep_count == 4:
+            one_rep_max_lb = (input_weight_lb * 4 * 0.033) + input_weight_lb # Modifies formula for 4 reps performed.
+            break
+        elif input_rep_count == 5:
+            one_rep_max_lb = (input_weight_lb * 5 * 0.033) + input_weight_lb # Modifies formula for 5 reps performed.
+            break
+        elif input_rep_count == 6:
+            one_rep_max_lb = (input_weight_lb * 6 * 0.033) + input_weight_lb # Modifies formula for 6 reps performed.
+            break
+    except ValueError:
+        print("Not a valid rep count, try again. ") # Retry input until 4, 5, or 6 is entered.
+        continue
 
 rpe_95_lb = one_rep_max_lb*0.95  # Calculate 95% rate of perceived effort.
 rpe_93_lb = one_rep_max_lb*0.93  # Calculate 93% rate of perceived effort.
@@ -26,8 +40,8 @@ rpe_75_lb = one_rep_max_lb*0.75  # Calculate 75% rate of perceived effort.
 while True:
     kg_or_lb = str.upper(input("Is this weight in kgs or lbs? ")) # Determines which units to display, rejects incorrect input.
     if kg_or_lb == "KGS":  # Selects kgs as unit.
-            print("You selected kgs.")
-            print(f"You lifted {input_weight_lb}kgs for 4-6 reps. Your 1RM is {one_rep_max_lb}kgs.")
+            print("You selected kgs. \n")
+            print(f"You lifted {input_weight_lb}kgs for {input_rep_count} reps. Your 1RM is {one_rep_max_lb}kgs. \n")
             print(f"RPE 95%: {rpe_95_lb}kgs for 2 reps.")
             print(f"RPE 93%: {rpe_93_lb}kgs for 3 reps.")
             print(f"RPE 90%: {rpe_90_lb}kgs for 4 reps.")
@@ -36,12 +50,12 @@ while True:
             print(f"RPE 83%: {rpe_83_lb}kgs for 7 reps.")
             print(f"RPE 80%: {rpe_80_lb}kgs for 8 reps.")
             print(f"RPE 77%: {rpe_77_lb}kgs for 9 reps.")
-            print(f"RPE 75%: {rpe_75_lb}kgs for 10 reps.")
+            print(f"RPE 75%: {rpe_75_lb}kgs for 10 reps. \n")
             break
 
     elif kg_or_lb == "LBS": # Selects lbs as unit.
-            print("You selected lbs.")
-            print(f"You lifted {input_weight_lb}lbs for 4-6 reps. Your 1RM is {one_rep_max_lb}lbs.")
+            print("You selected lbs. \n")
+            print(f"You lifted {input_weight_lb}lbs for {input_rep_count} reps. Your 1RM is {one_rep_max_lb}lbs. \n")
             print(f"RPE 95%: {rpe_95_lb}lbs for 2 reps.")
             print(f"RPE 93%: {rpe_93_lb}lbs for 3 reps.")
             print(f"RPE 90%: {rpe_90_lb}lbs for 4 reps.")
@@ -50,7 +64,7 @@ while True:
             print(f"RPE 83%: {rpe_83_lb}lbs for 7 reps.")
             print(f"RPE 80%: {rpe_80_lb}lbs for 8 reps.")
             print(f"RPE 77%: {rpe_77_lb}lbs for 9 reps.")
-            print(f"RPE 75%: {rpe_75_lb}lbs for 10 reps.")
+            print(f"RPE 75%: {rpe_75_lb}lbs for 10 reps. \n")
             break
 
     else:  # Inform user of valid unit input on bad input.
